326b63cab939ea332fd82c2a7ec52ffd653bca41
mlarco <mlarco94@gmail.com>
2022-08-13 20:52:10 -0300

    Added catch and loading conditionals

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,25 @@ import Article from "./Article";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setisPending] = useState(true);
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setisPending(false);
+                    setBlogs(data);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
+        }, 1000);
     }, []);
     return (
         <div>
+            {isPending && <div>Loading</div>}
             {blogs && <Article blogs={blogs} />}
         </div>
     );
